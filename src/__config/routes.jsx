b309356e85a1558f2222433e19399d0482210600
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-07-23 11:41:22 +0100

    save progress

--- src/__config/routes.jsx
+++ src/__config/routes.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import Query from '../handler/query';
 import TileHandler from '../handler/tile-handler';
 import FormHandler from '../handler/form-handler';
+import TreeHandler from '../handler/tree-handler';
 import createStatus from './forms/create.status';
+import searchStatus from './trees/search.status';
 
 const onMount = (props, state, onSuccess, onError) => {
     const data = (new Array(128)).fill(1).map((_, i) => ({
@@ -26,8 +28,13 @@ export default [
             />
     },
     {
-        path: '/new',
+        path: ['/new', '/add', '/status/new', '/status/add'],
         exact: true,
         component: () => <FormHandler {...createStatus} />,
     },
+    {
+        path: ['/view', '/tree', '/explore', '/status/:id/explore'],
+        exact: true,
+        component: () => <TreeHandler {...searchStatus} />,
+    }
 ]
