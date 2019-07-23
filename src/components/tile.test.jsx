5402057b66080e78a2dc194f55c93eb078b1b799
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-07-23 10:04:47 +0100

    fix consequenses of previous pr with case-sensitivity

--- src/components/tile.test.jsx
+++ src/components/tile.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import Tile from './tile_';
+import Tile from './tile';
 
 configure({ adapter: new Adapter() });
 
--- src/handlers/tile-handler.jsx
+++ src/handlers/tile-handler.jsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
-import Tile from '../components/tile_';
+import Tile from '../components/tile';
 
 export default class TileHandler extends PureComponent {
     constructor({ data }) {
