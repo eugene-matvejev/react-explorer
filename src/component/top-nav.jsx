920f02831514e2911e1ab97a7fe39b21792b1900
Arjun Yadav <arjun.prakash.yadav@gmail.com>
2019-08-02 12:42:43 +0100

    Adding the guthub logo link to repo on all site pages

--- src/component/top-nav.jsx
+++ src/component/top-nav.jsx
@@ -4,6 +4,7 @@ import PropTypes from 'prop-types';
 const TopNav = ({ className, ...props }) =>
     <nav className={`topnav ${className}`} {...props}>
         <img className="topnav__logo" src="/assets/img/placeholder.png" alt="logo" />
+        <a href="https://github.com/eugene-matvejev/react-explorer"><img className="topnav__github-logo" alt="GitHub Logomark" src="/assets/img/github-logo.png"></img></a>
     </nav>
 
 TopNav.propTypes = {
