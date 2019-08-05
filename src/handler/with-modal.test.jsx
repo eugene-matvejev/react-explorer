9bbe45e1bca1c501b51de7565f0cbb850e7bad9f
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-08-05 16:05:26 +0100

    implement withModal HOC

---
+++ src/handler/with-modal.test.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { configure, shallow, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import withModal from './with-modal';
+
+const { WrappedComponent: WithModal } = withModal(() => <div className="{{wrappedComponent}}" />);
+
+configure({ adapter: new Adapter() });
+
+describe('<WithModal/> as HOC', () => {
+    const props = {
+        location: {},
+        history: {
+            goBack: jest.fn(),
+        },
+        'data-cy': 'should be passed "as is"',
+        modals: [
+            {
+                path: ['/explore/:id'],
+                component: (props) => <div className="{{modal}}" />,
+            },
+        ],
+    };
+
+    describe('render', () => {
+        it('with default/required props', () => {
+            const c = shallow(<WithModal {...props} />);
+
+            expect(c).toMatchSnapshot();
+        });
+
+        it('with location.state.isModal as true', () => {
+            const c = shallow(<WithModal {...props} location={{ state: { isModal: true } }} />);
+
+            expect(c).toMatchSnapshot();
+        });
+    });
+});
