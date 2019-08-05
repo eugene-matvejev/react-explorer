e4497cbdefc29a063565e1fff15d43b601b039d8
Arjun Yadav <arjun.prakash.yadav@gmail.com>
2019-08-05 13:47:29 +0100

    Github logo link changed to include react router link

--- src/component/top-nav.jsx
+++ src/component/top-nav.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 const TopNav = ({ className, ...props }) =>
     <nav className={`topnav ${className}`} {...props}>
         <img className="topnav__logo" src="/assets/img/placeholder.png" alt="logo" />
-        <a href="https://github.com/eugene-matvejev/react-explorer"><img className="topnav__github-logo" alt="GitHub Logomark" src="/assets/img/github-logo.png"></img></a>
+        <Link to="//github.com/eugene-matvejev/react-explorer" target="_blank" >
+            <img className="topnav__github-logo" alt=" GitHub logo" src="/assets/img/github-logo.png"></img>
+        </Link>
     </nav>
 
 TopNav.propTypes = {
