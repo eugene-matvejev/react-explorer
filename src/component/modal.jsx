017ac6d569922ccad59625ff0cbba3d13caf1f76
Eugene Matvejev <eugene.matvejev@gmail.com>
2019-11-11 16:19:25 +0000

    fixes

--- src/component/modal.jsx
+++ src/component/modal.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const Modal = ({ 'data-cy': cy, className, onClose, children }) =>
-    <div className="modal-container">
+    <div className="modal_container">
         <section className={`modal ${className}`}>
-            <div className="modal-head">
-                <button className="modal-button" data-cy={`${cy}-modal-close`} onClick={onClose}>
+            <div className="modal_head">
+                <button className="modal_button--close" data-cy={`${cy}-modal-close`} onClick={onClose}>
                     ×
                 </button>
             </div>
-            <div className="modal-content">
+            <div className="modal_content">
                 {children}
             </div>
         </section>
