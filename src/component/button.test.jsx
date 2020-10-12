d4a3b348bb62a2555e958324952fcc0540db789a
Eugene Matvejev <eugene.matvejev@gmail.com>
2020-10-12 22:20:58 +0100

    try react testing liblary

--- src/component/button.test.jsx
+++ src/component/button.test.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+// import { setupServer } from 'msw/node'
+import { render, asFragment } from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect'
 import Button from './button';
-
-configure({ adapter: new Adapter() });
 
 describe('<Button/>', () => {
     const props = {
@@ -12,22 +11,14 @@ describe('<Button/>', () => {
 
     describe('render', () => {
         it('with default/required props', () => {
-            const c = shallow(<Button {...props} />);
+            const { container } = render(<Button {...props} />);
 
-            expect(c).toMatchSnapshot();
+            expect(container).toMatchSnapshot();
         });
+        it('with default/required props 2', () => {
+            const { asFragment } = render(<Button {...props} />);
 
-        describe('with optional props', () => {
-            [
-                ['className', '{{className}}'],
-                ['data-cy', '{{data-cy}}'], /** should be passed 'as is' */
-            ].forEach(([prop, v]) => {
-                it(`[::${prop}] as "${v}"`, () => {
-                    const c = shallow(<Button {...props} {...{ [prop]: v }} />);
-
-                    expect(c).toMatchSnapshot();
-                });
-            });
+            expect(asFragment()).toMatchSnapshot();
         });
     });
 });
